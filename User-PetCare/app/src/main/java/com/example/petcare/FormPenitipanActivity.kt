f967229cdc074d5d19f047360d76d1be1ab1f1ed
Khoirudin-Ansori <58926314+Khoirudin-Ansori@users.noreply.github.com>
2020-04-21 12:57:15 +0700

    second commit

---
+++ User-PetCare/app/src/main/java/com/example/petcare/FormPenitipanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormPenitipanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_penitipan)
+    }
+}
