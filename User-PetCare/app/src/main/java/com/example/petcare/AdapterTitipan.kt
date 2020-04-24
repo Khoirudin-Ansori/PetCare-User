3ea63be23f118200c99f090d611377f34fe5cf7d
Khoirudin-Ansori <58926314+Khoirudin-Ansori@users.noreply.github.com>
2020-04-24 19:48:49 +0700

    tampilan data titipan

---
+++ User-PetCare/app/src/main/java/com/example/petcare/AdapterTitipan.kt
@@ -0,0 +1,32 @@
+package com.example.petcare
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterTitipan(val TitipanList:ArrayList<Titipan>): RecyclerView.Adapter<AdapterTitipan.ViewHolder>() {
+    override fun onBindViewHolder(holder: AdapterTitipan.ViewHolder, position: Int) {
+
+        val titipan: Titipan=TitipanList[position]
+        holder?.nama_hewan?.text = titipan.nama_hewan
+        holder?.tanggal_penitipan?.text = titipan.tanggal_penitipan
+        holder?.tanggal_kembali?.text = titipan.tanggal_kembali
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterTitipan.ViewHolder {
+        val v= LayoutInflater.from(parent?.context).inflate(R.layout.list_titipan, parent, false)
+        return AdapterTitipan.ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return TitipanList.size
+    }
+    class  ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val nama_hewan= itemView.findViewById(R.id.nama_hewan) as TextView
+        val tanggal_penitipan = itemView.findViewById(R.id.tanggal_penitipan) as TextView
+        val tanggal_kembali= itemView.findViewById(R.id.tanggal_kembali) as TextView
+    }
+}
--- User-PetCare/app/src/main/java/com/example/petcare/DataTitipanActivity.kt
+++ User-PetCare/app/src/main/java/com/example/petcare/DataTitipanActivity.kt
@@ -2,11 +2,60 @@ package com.example.petcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import kotlinx.android.synthetic.main.activity_data_titipan.*
+import kotlinx.android.synthetic.main.activity_form_penitipan.*
+import kotlinx.android.synthetic.main.list_titipan.*
+import kotlinx.android.synthetic.main.list_titipan.nama_hewan
+import kotlinx.android.synthetic.main.list_titipan.tanggal_penitipan
+import org.json.JSONObject
 
 class DataTitipanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_titipan)
+        getdatatitipan()
+    }
+    fun getdatatitipan(){
+        val recyclerView = findViewById(R.id.recyclerTitipan) as RecyclerView
+        recyclerView.layoutManager= LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+        val titipan=ArrayList<Titipan>()
+        AndroidNetworking.get("http://192.168.1.248/Semester_4/Mobile_Programming_Terapan/UAS/API/pet_care/data_titipan.php")
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    Log.e("_kotlinResponse", response.toString())
+
+                    val jsonArray = response!!.getJSONArray("result")
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        Log.e("_kotlinTitle", jsonObject.optString("nama_hewan"))
+//                        nama_hewan.setText(jsonObject.optString("nama_hewan"))
+//                        tanggal_penitipan.setText(jsonObject.optString("tanggal_penitipan"))
+//                        t_kembali.setText(jsonObject.optString("tanggal_kembali"))
+
+                        // txt1.setText(jsonObject.optString("shubuh"))
+                        var isi1=jsonObject.optString("nama_hewan").toString()
+                        var isi2=jsonObject.optString("tanggal_penitipan").toString()
+                        var isi3=jsonObject.optString("tanggal_kembali").toString()
+                        titipan.add(Titipan("$isi1", "$isi2", "$isi3"))
+                    }
+                    val adapter=AdapterTitipan(titipan)
+                    recyclerView.adapter=adapter
+                }
+                override fun onError(anError: ANError) {
+                    Log.i("_err", anError.toString())
+                }
+            })
     }
 }
